c99c2b1d53c19fbaae3851674091242c2839c482
Ethan Brown <eb934053@gmail.com>
2025-01-02 22:18:34 -0600

    Added functionality with useEffect and useState. Modified CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,53 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { getAllTickets } from "./services/ticketServices"
+import "./App.css"
 
 export const App = () => {
-  const [count, setCount] = useState(0) 
-  const handleButtonClick = () => {
-    setCount(count + 1)
-    console.log(count)
-  }
+  const [allTickets, setAllTickets] = useState([])
+  const [showEmergencyOnly, setShowEmergencyOnly] = useState(false)
+  const [filteredTickets, setFilteredTickets] = useState([])
+
+  useEffect(() => {
+    getAllTickets().then(ticketsArray => {
+      setAllTickets(ticketsArray)
+      console.log("tickets set")
+    })
+  }, [])
+
+  useEffect(() => {
+    if(showEmergencyOnly) {
+      const emergencyTickets = allTickets.filter((ticket) => ticket.emergency === true)
+      setFilteredTickets(emergencyTickets)
+    }
+    else {
+      setFilteredTickets(allTickets)
+    }
+  }, [showEmergencyOnly, allTickets])
 
   return (
     <>
-      <h1>Hello!</h1>
-      <div>This is amazing!</div>
-      <button className="btn-secondary" onClick={handleButtonClick}>Click me!</button>
-      <div>Count: {count}</div>
+      <div className="tickets-container"></div>
+      <h2>Tickets</h2>
+      <article className="tickets">
+        <div>
+          <button className="filter-btn btn-primary" onClick={() => {setShowEmergencyOnly(true)}}>Emergency!</button>
+          <button className="filter-btn btn-secondary" onClick={() => {setShowEmergencyOnly(false)}}>Show All</button>
+        </div>
+        {filteredTickets.map(ticket => {
+          return (
+            <section className="ticket" key={ticket.id}>
+              <header className="ticket-info">#{ticket.id}</header>
+              <div>{ticket.description}</div>
+              <footer>
+                <div>
+                  <div className="ticket-info">emergency</div>
+                  <div>{ticket.emergency ? "Yes" : "No"}</div>
+                </div>
+              </footer>
+            </section>
+        )
+        })}
+      </article>
     </>
   ) 
 }
