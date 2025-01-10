93fbd09f8df8bca4c06f5a59f3cd8d8d2c849457
Ethan Brown <eb934053@gmail.com>
2025-01-10 09:29:59 -0600

    Added Navigation Bar functionality to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { TicketList } from "./components/tickets/TicketList"
 import { CustomerList } from "./components/customers/customersList"
 import { EmployeeList } from "./components/employees/employeeList"
+import { Outlet, Route, Routes } from "react-router-dom"
+import { NavBar } from "./components/nav/NavBar"
 
 
 export const App = () => {
-  return <> 
-  {/* <TicketList /> */} 
-  {/* <CustomerList /> */}
-  <EmployeeList />
-  </>
+  return <Routes>
+    <Route 
+      path="/" 
+      element={
+        <>
+          <NavBar />
+          <Outlet />
+        </>
+      }
+    > 
+      <Route path="tickets" element={<TicketList />} />
+      <Route path="customers" element={<CustomerList />} />
+      <Route path="employees" element={<EmployeeList />} />
+    </Route> 
+  </Routes>
 }
